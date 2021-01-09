c4982dacb6e3201943a3d67bc889abb069e9eef5
JayapradaBiswal <jayaprada.nalanda@gmail.com>
2021-01-09 11:31:07 +0530

    ByType done

---
+++ Spring_Di_Autoweiring_ByType/src/com/spring/basic/Runner.java
@@ -0,0 +1,19 @@
+package com.spring.basic;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		ApplicationContext contianer = new ClassPathXmlApplicationContext("context.xml");
+		System.out.println("container created");
+
+		Husband kamlesh = contianer.getBean("kamlesh", Husband.class);
+		System.out.println(kamlesh);
+		
+		Husband akash = contianer.getBean("akash", Husband.class);
+		System.out.println(akash);
+	}
+
+}
