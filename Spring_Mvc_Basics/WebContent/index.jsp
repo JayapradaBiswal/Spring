59f7a9c5e80a9ef2712cf2803beeafa5b7527724
JayapradaBiswal <jayaprada.nalanda@gmail.com>
2021-01-09 11:54:15 +0530

    mvc done

---
+++ Spring_Mvc_Basics/WebContent/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<head>
+</head>
+<body>
+<h1>hello spring mvc</h1>
+</body>
+</html>
---
+++ Spring_Mvc_Basics/src/com/test/mvc/WelcomeControler.java
@@ -0,0 +1,14 @@
+package com.test.mvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WelcomeControler
+{
+
+	public WelcomeControler()
+	{
+		System.out.println("Welcome controller created");
+	}
+
+}
