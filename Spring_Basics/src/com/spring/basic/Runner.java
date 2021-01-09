fbb19630f3409103bcc03feb5d6c0f8a4448ee07
JayapradaBiswal <jayaprada.nalanda@gmail.com>
2021-01-09 11:18:16 +0530

    basics done

---
+++ Spring_Basics/src/com/spring/basic/Runner.java
@@ -0,0 +1,20 @@
+package com.spring.basic;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Runner
+{
+	public static void main(String[] args) 
+	{
+		//create spring container
+		//Step 1
+ApplicationContext container=
+         new ClassPathXmlApplicationContext("Spring-core.xml");//add Spring-core.xml in src folder
+   System.out.println("Container Created");
+   
+   Student st=container.getBean("std", Student.class);
+   st.study();
+	}
+
+}
---
+++ Spring_Basics/src/com/spring/basic/Student.java
@@ -0,0 +1,15 @@
+package com.spring.basic;
+
+public class Student
+{
+
+	public void study()
+	{
+		System.out.println("method");
+	}
+	public Student() 
+	{
+		System.out.println("Student object is created");
+	}
+
+}
