f89e1462afb4b9fb81c5dd1e05de00ea5c672068
JayapradaBiswal <jayaprada.nalanda@gmail.com>
2021-01-09 11:41:18 +0530

    committed

---
+++ Spring_Annotation_Basic/src/com/spring/basic/Person.java
@@ -0,0 +1,11 @@
+package com.spring.basic;
+import org.springframework.stereotype.Component;
+@Component
+public class Person 
+{
+public	Person()
+{
+	System.out.println("--person obj created----");
+}
+
+}
